41bc4f350b1b068fb3284cb1e07c48d954f7eebc
emil.a.johansson <emil.a.johansson@gmail.com>
2022-09-19 13:18:22 +0200

    Add project files.

---
+++ Models/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo_Lexicon_Assignment.Models
+{
+    internal class Person
+    {
+        private readonly int Id;
+        private string? FirstName;
+        private string? LastName;
+
+        public Person(int id, string firstName, string lastName)
+        {
+            this.Id = ++Id;
+            if (!string.IsNullOrWhiteSpace(firstName))
+            {
+                this.FirstName = firstName;
+            } else throw new ArgumentNullException();
+            if (!string.IsNullOrWhiteSpace(lastName))
+            {
+                this.LastName = lastName;
+            } else throw new ArgumentNullException();
+        }
+        public string fullName {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+        public int getID {
+            get
+            {
+                return Id;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDo_Lexicon_Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
